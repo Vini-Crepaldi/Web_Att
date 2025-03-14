bef2cd2fb990c5890847d70d60ed0814e6879c82
Crepaldi <108742768+Vini-Crepaldi@users.noreply.github.com>
2025-03-13 22:25:35 -0300

    Atividade 3

---
+++ atividade_3/arrayPares.js
@@ -0,0 +1,12 @@
+export default function arrayPares(N) {
+    const numerosPares = []; 
+
+    for (let i = 1; i <= N; i++) {
+        numerosPares.push(i * 2); 
+    }
+
+    return numerosPares; 
+}
+
+
+
---
+++ atividade_3/index.js
@@ -0,0 +1,6 @@
+import arrayPares from "./arrayPares.js";
+
+let N = 10;
+const resultado = arrayPares(N); 
+
+console.log(`Os primeiros ${N} números pares são: `+ resultado);
